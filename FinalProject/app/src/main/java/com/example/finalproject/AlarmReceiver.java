a8a80319408a40daf326e65723dfdf618cb514f7
beenson <benson070707@yahoo.com.tw>
2021-06-14 03:10:35 +0800

    notifi on time

---
+++ FinalProject/app/src/main/java/com/example/finalproject/AlarmReceiver.java
@@ -0,0 +1,47 @@
+package com.example.finalproject;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Vibrator;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    private String NotifiChannel = "Alarm";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        //Vibrate
+        Vibrator vibrator = (Vibrator)context.getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.vibrate(2000);
+
+        //Notification
+        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        Intent _intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, _intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, NotifiChannel);
+        builder.setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("Alarm")
+                .setContentText("Time is up!!")
+                .setAutoCancel(true)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent)
+                .setDefaults(NotificationCompat.DEFAULT_ALL);
+        mNotificationManager.notify(0, builder.build());
+
+        //Ringtone
+        Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        Ringtone r = RingtoneManager.getRingtone(context, notification);
+        r.play();
+    }
+}
--- FinalProject/app/src/main/java/com/example/finalproject/MainActivity.java
+++ FinalProject/app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,26 +1,47 @@
 package com.example.finalproject;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
+import java.util.WeakHashMap;
 
 public class MainActivity extends AppCompatActivity {
     private SharedPreferences mPreferences;
     private String sharedPrefFile = "com.example.android.finalproject";
     private String MODE_KEY = "mode";
+    private String HOUR_KEY = "hour", MINUTE_KEY = "minute";
     private ArrayList<String> WEEKDAY_KEY = new ArrayList<>();
+    private NotificationManager mNotificationManager;
+    private String NotifiChannel = "Alarm";
     ArrayList<LinearLayout> mode = new ArrayList<>();
     Map<String, Boolean> weekday = new HashMap<>();
     int currentMode;
+    TimePicker timePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,21 +49,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Map<String, Button> weekdayBtn = new HashMap<>();
+        weekdayBtn.put(getString(R.string.sundayTag), findViewById(R.id.btnSunday));
         weekdayBtn.put(getString(R.string.mondayTag), findViewById(R.id.btnMonday));
         weekdayBtn.put(getString(R.string.tuesdayTag), findViewById(R.id.btnTuesday));
         weekdayBtn.put(getString(R.string.wednesdayTag), findViewById(R.id.btnWednesday));
         weekdayBtn.put(getString(R.string.thursdayTag), findViewById(R.id.btnThursday));
         weekdayBtn.put(getString(R.string.fridayTag), findViewById(R.id.btnFriday));
         weekdayBtn.put(getString(R.string.saturdayTag), findViewById(R.id.btnSaturday));
-        weekdayBtn.put(getString(R.string.sundayTag), findViewById(R.id.btnSunday));
         WEEKDAY_KEY = new ArrayList<String>(
-                Arrays.asList(getString(R.string.mondayTag),
+                Arrays.asList(getString(R.string.sundayTag),
+                        getString(R.string.mondayTag),
                         getString(R.string.tuesdayTag),
                         getString(R.string.wednesdayTag),
                         getString(R.string.thursdayTag),
                         getString(R.string.fridayTag),
-                        getString(R.string.saturdayTag),
-                        getString(R.string.sundayTag)));
+                        getString(R.string.saturdayTag)));
 
         mPreferences = getSharedPreferences(sharedPrefFile, MODE_PRIVATE);
         currentMode = mPreferences.getInt(MODE_KEY, 0);
@@ -56,6 +77,16 @@ public class MainActivity extends AppCompatActivity {
         mode.add(findViewById(R.id.modeMathLayout));
         mode.add(findViewById(R.id.modeMemorizeLayout));
         mode.get(currentMode).setBackground(getDrawable(R.drawable.mode_selected));
+
+        timePicker = (TimePicker)findViewById(R.id.timePicker);
+        timePicker.setHour(mPreferences.getInt(HOUR_KEY, 0));
+        timePicker.setMinute(mPreferences.getInt(MINUTE_KEY, 0));
+        timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                setAlarm();
+            }
+        });
     }
 
     @Override
@@ -64,6 +95,8 @@ public class MainActivity extends AppCompatActivity {
 
         SharedPreferences.Editor preferencesEditor = mPreferences.edit();
         preferencesEditor.putInt(MODE_KEY, currentMode);
+        preferencesEditor.putInt(HOUR_KEY, timePicker.getHour());
+        preferencesEditor.putInt(MINUTE_KEY, timePicker.getMinute());
         WEEKDAY_KEY.forEach((value) -> preferencesEditor.putBoolean(value, weekday.get(value)));
         preferencesEditor.apply();
     }
@@ -78,5 +111,51 @@ public class MainActivity extends AppCompatActivity {
         String day = view.getTag().toString();
         weekday.put(day, !weekday.get(view.getTag()));
         view.setBackgroundColor(getColor(weekday.get(day) ? R.color.weekdaySelected : R.color.weekdayUnselected));
+    }
+
+    public void setAlarm() {
+        Intent intent = new Intent(MainActivity.this, AlarmReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 24444, intent, 0);
+
+        //clear previous alarm
+        AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+
+        //check there is on at least one weekday
+        Boolean active = false;
+        for (Map.Entry<String, Boolean> entry : weekday.entrySet()) {
+            active |= entry.getValue();
+        }
+        if(!active)
+            return;
+
+        //set alarm time
+        Calendar alarm = Calendar.getInstance();
+
+        alarm.set(Calendar.HOUR_OF_DAY, timePicker.getHour());
+        alarm.set(Calendar.MINUTE, timePicker.getMinute());
+        alarm.set(Calendar.SECOND, 0);
+
+        //get next weekday
+        while (alarm.before(Calendar.getInstance()) || !weekday.get(WEEKDAY_KEY.get(alarm.get(Calendar.DAY_OF_WEEK) - Calendar.SUNDAY))) {
+            alarm.add(Calendar.DATE, 1);
+        }
+        //Toast.makeText(this, alarm.get(Calendar.DATE) + "", Toast.LENGTH_SHORT).show();
+
+        alarmManager.set(AlarmManager.RTC_WAKEUP, alarm.getTimeInMillis(), pendingIntent);
+
+        mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        createNotificationChannel();
+    }
+
+    private void createNotificationChannel() {
+        mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel notificationChannel = new NotificationChannel(NotifiChannel, "test", NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.WHITE);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("desc");
+            mNotificationManager.createNotificationChannel(notificationChannel);
+        }
     }
 }
